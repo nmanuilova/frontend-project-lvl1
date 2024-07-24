06cefe81438765d89262a0edf18a1cf32f1b4bfe
nmanuilova <nmanuilova@fix.ru>
2024-07-24 19:54:51 +0300

    fix

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,24 +1,23 @@
 import playGame from '../index.js';
 
-
 const isPrime = (number) => {
-    if (number <= 1) 
-        return false; 
-    if (number == 2 || number == 3) 
-        return true; 
-    if (number % 2 == 0 || number % 3 == 0) 
-        return false; 
-    for (var i = 5; i <= Math.sqrt(number); i = i + 6) 
-        if (number % i == 0 || number % (i + 2) == 0) 
-            return false; 
-    return true; 
-}
+  if (number <= 1) {
+    return false;
+  }
+
+  for (let i = 2; i <= Math.sqrt(number); i += 1) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
 
 const generateQuestionAndAnswer = () => {
-    const randomNumber = Math.floor(Math.random() * 10);
+  const randomNumber = Math.floor(Math.random() * 10);
   const expectedAnswer = isPrime(randomNumber) ? 'yes' : 'no';
   return [randomNumber, expectedAnswer];
-}
+};
 
 export default () => playGame(
   'Answer "yes" if given number is prime. Otherwise answer "no".',
--- src/games/progression.js
+++ src/games/progression.js
@@ -1,14 +1,13 @@
 import playGame from '../index.js';
 
-
-const progressionLength = 7; 
+const progressionLength = 7;
 
 const generateProgression = () => {
   const step = Math.floor(Math.random() * 10) + 1;
   let previousNumber = Math.floor(Math.random() * 10);
   const progression = [`${previousNumber}`];
 
-  for (let i = 1; i < progressionLength; i +=1) {
+  for (let i = 1; i < progressionLength; i += 1) {
     previousNumber += step;
     progression.push(`${previousNumber}`);
   }
@@ -17,13 +16,13 @@ const generateProgression = () => {
 };
 
 const generateQuestionAndAnswer = () => {
-    const progression = generateProgression();
-    const indexOfItemWithPoint = Math.floor(Math.random() * (progressionLength - 1));
-    const expectedAnswer = progression[indexOfItemWithPoint];
-    progression[indexOfItemWithPoint] = '..';
+  const progression = generateProgression();
+  const indexOfItemWithPoint = Math.floor(Math.random() * (progressionLength - 1));
+  const expectedAnswer = progression[indexOfItemWithPoint];
+  progression[indexOfItemWithPoint] = '..';
 
-    return [progression.join(' '), expectedAnswer];
-}
+  return [progression.join(' '), expectedAnswer];
+};
 
 export default () => playGame(
   'What number is missing in the progression?',
